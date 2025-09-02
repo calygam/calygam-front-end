1148622ecb4fc36f1aa10c3e4e7ec56f43b009ca
DevCaioBO <caiobuenooliveira1210@gmail.com>
2025-09-02 08:50:27 -0300

    feat:teste

--- tccfront/src/api/api.js
+++ tccfront/src/api/api.js
@@ -5,7 +5,7 @@ const api =axios.create({
     //baseURL:'http://localhost:8080/'
 })
 
-
+//aaaaa
 api.interceptors.request.use(
     (config)=>{
         const token = localStorage.getItem('token')
