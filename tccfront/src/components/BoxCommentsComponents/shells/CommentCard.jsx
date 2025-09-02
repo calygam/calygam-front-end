9de1a8dd5df9d237c78f8281876218458cadb162
DevCaioBO <caiobuenooliveira1210@gmail.com>
2025-09-02 10:48:51 -0300

    feat:batoso56

--- tccfront/src/components/BoxCommentsComponents/shells/CommentCard.jsx
+++ tccfront/src/components/BoxCommentsComponents/shells/CommentCard.jsx
@@ -22,7 +22,7 @@ export default function CommentCard() {
             {!loading&& msgState.dataMsg.messages.length==0?<p className='text-sm text-gray-600'>*Sem comentários no momento</p>:
             <ul className='flex flex-col max-w-[350px] pr-2  gap-y-2  '>
                 {msgState.dataMsg.messages.map(msg => (
-                    <div key={msg.messageActivityId}>
+                    <div className='flex flex-wrap max-w-[350px]' key={msg.messageActivityId}>
                    <DetailCommentCard msg={msg} />
                    
                    
